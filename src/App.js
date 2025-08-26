982d820a8ec60d0f57a60b532a3234707f1f35e4
Breno Guimarães <devbreno4@gmail.com>
2025-08-26 00:17:45 -0300

    componentes e useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Idade from "./components/Idade";
+import {useState} from 'react'; 
 
+import React from "react";
 function App() {
+  //useState é um hook para adicionar dinamismo na app 
+  const [name,setNome] = useState('Breno Guimarães');
+
+  function changeNameHook(name){
+      setNome(name)
+  }
+  
+  //sempre que utilizamos useState passamos a variavel e o evento que iramos setar essa variavel 
+  // o evento que que vamos chamar essa variavel sempre  definimos atraves de uma função 
+  // essa função é chamada dentro de um evento onLoad 
+  //sempre que queremos acessar o JS dentro do return utilizamos '{}'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TESTE REACT CARALHO</h1>
+      <Nome nome="Pedro"/>
+      <br/>
+      <Idade idade="20" ano_nascimento={2005}/>
+       <br/>
+      <Idade idade="21" ano_nascimento={2004}/>
+      <br/>
+      <h2>Property Hook, Name - {name} </h2>
+      <button onClick={() => changeNameHook('Lucas Ryan Gomes')}>
+        Mudar Estado
+      </button>
     </div>
   );
 }
 
 export default App;
+
+//COMPONENTE SEMPRE TEM QUE TER LETRA MAISCULA NO INICIO 
+//como chamamos na mesma app n precisamos utilizar o export default 
+function Nome(){
+  return(
+    <span>Nome é Breno</span>
+  );
+}
---
+++ src/components/Idade.js
@@ -0,0 +1,18 @@
+
+function Idade({idade, ano_nascimento}){
+    return(
+        <span>Idade é  {idade} - Ano de Nascimento = {ano_nascimento} </span>
+        
+
+    );
+}
+
+export default Idade; 
+
+/*
+    Propridade basicamente é um array 
+    let props = {
+        idade : 21, 
+        ano_nascimento : 2004
+    } 
+*/ 
