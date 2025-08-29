f0ecd366cdd89913584c126fa6f16d6ae2b95a3b
Breno Guimarães <devbreno4@gmail.com>
2025-08-29 19:54:34 -0300

    aprendendo a manipular formularios

--- src/App.js
+++ src/App.js
@@ -6,14 +6,31 @@ function App() {
   //useState é um hook para adicionar dinamismo na app 
   const [name,setNome] = useState('Breno Guimarães');
 
+  const[nameUser, setNameUser] = useState('') 
+  const[emailUser, setEmailUser] = useState('')
+  const[ageUser, setAgeUser] = useState(0)
+
+  const [user, setUser] = useState({})
+
   function changeNameHook(name){
       setNome(name)
+  }
+
+  function handleUser(e){
+      e.preventDefault() 
+
+      alert("User Cadastrado com Sucesso")
+      setUser({
+        nome : nameUser, 
+        email : emailUser, 
+        age: ageUser
+      })
   }
   
   //sempre que utilizamos useState passamos a variavel e o evento que iramos setar essa variavel 
   // o evento que que vamos chamar essa variavel sempre  definimos atraves de uma função 
   // essa função é chamada dentro de um evento onLoad 
-  //sempre que queremos acessar o JS dentro do return utilizamos '{}'
+  //sempre que queremos acessar o JS dentro do return utilizamos ''
   return (
     <div className="App">
       <h1>TESTE REACT CARALHO</h1>
@@ -27,6 +44,42 @@ function App() {
       <button onClick={() => changeNameHook('Lucas Ryan Gomes')}>
         Mudar Estado
       </button>
+      <br/>
+      <br/>
+      <br/>
+      {/* forms  */}  
+
+      <h2>CADASTRO DE USUÁRIO</h2>
+      <form onSubmit={handleUser}>
+          <label for="">Nome</label>
+          <input type="text" 
+            value={nameUser} 
+            onChange={(e) => {setNameUser(e.target.value)}}></input>
+          <br/>
+          <label for="">email</label>
+          <input type="email" value={emailUser} onChange={(e)=>{setEmailUser(e.target.value)}}></input>
+          <br/>
+           <label for="">Idade</label>
+          <input type="number" 
+            value={ageUser} 
+            onChange={(e)=>{setAgeUser(e.target.value)}}>
+            </input>
+            <br/>
+            <br/>
+            <button type="submit">Cadastrar</button>
+      </form>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <div>
+          <h2>Resultado</h2>
+            <span>Nome é {user.nome}</span> <br/>
+            <span>Email é {user.email}</span> <br/>
+            <span>Idade é {user.age}</span> <br/>
+
+        </div>
     </div>
   );
 }
